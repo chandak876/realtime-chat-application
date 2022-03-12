a8ccd243cb6c5c2bad216a84cca7682ca9d6aa72
chandak876 <achandak@tekion.com>
2022-03-12 20:21:20 +0530

    first commit

---
+++ utils/socket.js
@@ -0,0 +1,39 @@
+const {getUsers, users=[]} = require('./getUsers');
+
+//Socket connection
+function socket(io) {
+    io.on('connection', (socket) => {
+        socket.on('joined-user', (data) =>{
+            //Storing users connected in a room in memory
+            var user = {};
+            user[socket.id] = data.username;
+            if(users[data.roomname]){
+                users[data.roomname].push(user);
+            }
+            else{
+                users[data.roomname] = [user];
+            }
+            
+            //Joining the Socket Room
+            socket.join(data.roomname);
+    
+            //Emitting New Username to Clients
+            io.to(data.roomname).emit('joined-user', {username: data.username});
+    
+            //Send online users array
+            io.to(data.roomname).emit('online-users', getUsers(users[data.roomname]))
+        })
+    
+        //Emitting messages to Clients
+        socket.on('chat', (data) =>{
+            io.to(data.roomname).emit('chat', {username: data.username, message: data.message});
+        })
+    
+        //Broadcasting the user who is typing
+        socket.on('typing', (data) => {
+            socket.broadcast.to(data.roomname).emit('typing', data.username)
+        })
+    })
+}
+
+module.exports = socket;
